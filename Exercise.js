d3da275e5ed329183c83aa90b8ebe98aec12a410
HamamPriyatmoko <115150198+HamamPriyatmoko@users.noreply.github.com>
2023-06-19 22:39:05 +0700

    Add files via upload

---
+++ Exercise.js
@@ -0,0 +1,11 @@
+video.addEventListener('click', function video() {
+  alert('ini video Arash Buana');
+});
+
+audio.addEventListener('click', function audio() {
+  alert('ini Audio Sigma');
+});
+
+gambar.addEventListener('click', function gambar() {
+  alert('ini gambar cowo sigma');
+});
